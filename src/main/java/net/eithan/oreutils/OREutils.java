4c36bd80fb31c8bb1388aa0904122d05fdeefa6a
Eithan Zur <eithanz713@gmail.com>
2023-10-23 16:25:30 +0300

    setup the mod

---
+++ src/main/java/net/eithan/oreutils/OREutils.java
@@ -0,0 +1,14 @@
+package net.eithan.oreutils;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OREutils implements ModInitializer {
+	public static final String MOD_ID = "oreutils";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/eithan/oreutils/OREutilsClient.java
@@ -0,0 +1,10 @@
+package net.eithan.oreutils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class OREutilsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
